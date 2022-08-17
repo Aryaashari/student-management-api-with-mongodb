9567b53da0a38e0d8ce96318f1d198631d77331a
Arya <aryaashari100@gmail.com>
2022-08-17 18:30:59 +0700

    create book controller and add to route

---
+++ app/controller/BookController.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace Student\Management\Controller;
+
+use Student\Management\App\View;
+
+class BookController {
+
+    public function getBook() {
+    }
+
+    public function createBook() {
+    }
+
+    public function updateBook() {
+    }
+
+    public function deleteBook() {
+    }
+
+}
