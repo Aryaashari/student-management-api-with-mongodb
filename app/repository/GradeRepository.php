1b2188dd60853accf02f7b35d6334b1f4ff43f90
Arya <aryaashari100@gmail.com>
2022-08-19 11:11:51 +0700

    create GradeRepository

---
+++ app/repository/GradeRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Student\Management\Repository;
+
+use Student\Management\Entity\Grade;
+
+class GradeRepository {
+
+
+    // public function findAll() : array {}
+
+    // public function findById(int $id) : ?Grade {}
+
+    // public function findByStudentId(int $studentId) : ?Grade {}
+
+    // public function create(Grade $grade) : Grade {}
+
+    // public function update(Grade $grade) : Grade {}
+
+    // public function delete(int $id) : bool {}
+
+
+}
