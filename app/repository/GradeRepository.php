d9d604a7ec6b09241a4939524671b24933608223
Arya <aryaashari100@gmail.com>
2022-08-19 11:11:59 +0700

    create GradeRepository

--- app/repository/GradeRepository.php
+++ app/repository/GradeRepository.php
@@ -2,10 +2,17 @@
 
 namespace Student\Management\Repository;
 
+use Student\Management\Config\Database;
 use Student\Management\Entity\Grade;
 
 class GradeRepository {
 
+    private \PDO $dbConn;
+
+    public function __construct()
+    {
+        $this->dbConn = Database::getConnection();
+    }
 
     // public function findAll() : array {}
 
