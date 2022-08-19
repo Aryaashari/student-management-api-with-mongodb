55bab43d4c27b1467b9c1086b928a8a3639eb564
Arya <aryaashari100@gmail.com>
2022-08-19 15:26:54 +0700

    create function to find all grade in grade service

--- app/service/GradeService.php
+++ app/service/GradeService.php
@@ -2,6 +2,8 @@
 
 namespace Student\Management\Service;
 
+use Student\Management\Model\GradeRequest;
+use Student\Management\Model\GradeResponse;
 use Student\Management\Repository\GradeRepository;
 
 class GradeService {
@@ -12,5 +14,22 @@ class GradeService {
     {
         $this->gradeRepo = $gradeRepo;
     }
+
+    public function findAllGrade() : array {
+        try {
+            $grades = $this->gradeRepo->findAll();
+            return $grades;
+        } catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
+    // public function findGradeById(int $id) : GradeResponse {}
+
+    // public function findGradeByStudentId(int $studentId) : GradeResponse {}
+
+    // public function updateGrade(GradeRequest $request) : GradeResponse {}
+
+    // public function deleteGrade(int $id) : bool {}
 
 }
---
+++ test/service/gradeServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Student\Management\Service;
+
+use PHPUnit\Framework\TestCase;
+use Student\Management\Config\Database;
+use Student\Management\Entity\Student;
+use Student\Management\Repository\GradeRepository;
+use Student\Management\Repository\StudentRepository;
+
+class gradeServiceTest  extends TestCase {
+
+    private GradeRepository $gradeRepo;
+    private GradeService $gradeService;
+    private StudentRepository $studentRepo;
+
+    public function setUp() : void {
+        $this->gradeRepo = new GradeRepository;
+        $this->gradeService = new GradeService($this->gradeRepo);
+        $this->studentRepo = new StudentRepository(Database::getConnection());
+        $this->gradeRepo->deleteAllData();
+    }
+
+    public function testFindAllGradeEmpty() {
+        $grades = $this->gradeService->findAllGrade();
+        $this->assertIsArray($grades);
+        $this->assertEmpty($grades);
+    }
+
+    // public function testFindAllGradeNotEmpty() {
+    //     $student = $this->studentRepo->create(new Student(null, "Arya", 18, "L"));
+    //     $grades = $this->gradeService->findAllGrade();
+    //     $this->assertIsArray($grades);
+    //     $this->assertTrue(count($grades) > 0);
+    // }
+
+}
