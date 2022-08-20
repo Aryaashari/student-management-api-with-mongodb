5e63b9f3194185715c44fe763beffad74e0656e7
Arya <aryaashari100@gmail.com>
2022-08-20 08:31:16 +0700

    create all function in GradeController

--- app/controller/GradeController.php
+++ app/controller/GradeController.php
@@ -14,4 +14,14 @@ class GradeController {
         $this->gradeService = new GradeService(new GradeRepository);
     }
 
+    public function findAllGrade() : void {}
+
+    public function findDetailGrade(int $id) : void {}
+
+    public function createGrade() : void {}
+
+    public function updateGrade() : void {}
+
+    public function deleteGrade() : void {}
+
 }
